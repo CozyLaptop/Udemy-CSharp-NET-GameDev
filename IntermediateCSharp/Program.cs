b7c7f3351e6da5c5862283c5e6c4e07ef59fa673
Albert <albert.j.dominguez21@gmail.com>
2022-01-19 09:12:40 -0600

    Enum practice

---
+++ IntermediateCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntermediateCSharp
+{
+    enum Meal
+    {
+        Breakfast,
+        Lunch,
+        Dinner
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meal mostImportantMeal = Meal.Breakfast;
+
+            if(mostImportantMeal == Meal.Breakfast)
+            {
+                Console.WriteLine("eggs and bacon");
+            }
+            Console.WriteLine((Meal)0);
+            Console.WriteLine((int)Meal.Lunch);
+            Console.WriteLine((int)Meal.Dinner);
+        }
+    }
+}
