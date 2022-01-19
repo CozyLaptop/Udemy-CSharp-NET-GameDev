82a915963fc14cb7686d87ae55b3625e3fa78dd6
Albert <albert.j.dominguez21@gmail.com>
2022-01-19 12:15:32 -0600

    init

---
+++ Shooting-RPG/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootingRPG
+{
+    public class Player
+    {
+        public Player()
+        {
+        }
+    }
+}
